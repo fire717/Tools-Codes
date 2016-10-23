b382230aaf51c7743004604f4f70e039ea4b150a
Fire <fire15@126.com>
2016-10-23 16:00:15 +0800

    big change

---
+++ Project/企业官网 ZCZB/scripts/banner.js
@@ -0,0 +1,8 @@
+$(function() {
+    $(".flexslider").flexslider({
+    	animation:"slide",
+		slideshowSpeed: 4000, //展示时间间隔ms
+		animationSpeed: 500, //滚动时间ms
+		slideshow: true //是否支持触屏滑动
+	});
+});	
